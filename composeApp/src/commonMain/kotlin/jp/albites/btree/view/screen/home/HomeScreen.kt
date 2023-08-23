255531c1006be7dfb64629db7e14389fe844d656
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-08-23 17:11:00 +0900

    Update edit menu

--- composeApp/src/commonMain/kotlin/jp/albites/btree/view/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/jp/albites/btree/view/screen/home/HomeScreen.kt
@@ -1,11 +1,18 @@
 package jp.albites.btree.view.screen.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddLink
@@ -13,7 +20,6 @@ import androidx.compose.material.icons.filled.CreateNewFolder
 import androidx.compose.material.icons.filled.DeleteSweep
 import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -27,7 +33,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -39,7 +47,6 @@ import jp.albites.btree.view.screen.home.dicretory.DirectoryDialog
 import jp.albites.btree.view.screen.home.edit.EditDialog
 import jp.albites.btree.view.screen.setting.SettingScreen
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import view.components.explorer.Explorer
 
 class HomeScreen(val openUrl: (String) -> Unit) : Screen {
@@ -73,56 +80,55 @@ class HomeScreen(val openUrl: (String) -> Unit) : Screen {
                     )
                 },
                 bottomBar = {
-                    BottomAppBar {
-                        IconButton(enabled = true, onClick = { showDeleteDialog = true }) {
-                            Icon(
-                                imageVector = Icons.Default.DeleteSweep,
-                                contentDescription = "Delete",
-                                modifier = Modifier.size(28.dp)
-                            )
-                        }
-
-                        IconButton(onClick = { showEditDialog = true }) {
-                            Icon(
-                                imageVector = Icons.Default.EditNote,
-                                contentDescription = "Edit",
-                                modifier = Modifier.size(28.dp)
-                            )
-                        }
-
-                        IconButton(
-                            enabled = state.selectedFile.isDirectory,
-                            onClick = { showBookmarkDialog = true }
+                    Box(modifier = Modifier.fillMaxWidth()) {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            modifier = Modifier
+                                .padding(vertical = 16.dp)
+                                .align(Alignment.Center)
+                                .background(
+                                    color = Color.LightGray.copy(alpha = 0.3f),
+                                    shape = RoundedCornerShape(8.dp)
+                                )
+                                .padding(horizontal = 16.dp)
                         ) {
-                            Icon(
-                                imageVector = Icons.Default.AddLink,
-                                contentDescription = "AddLink",
-                                modifier = Modifier.size(28.dp)
-                            )
-                        }
-
-                        IconButton(
-                            enabled = state.selectedFile.isDirectory,
-                            onClick = { showDirectoryDialog = true }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.CreateNewFolder,
-                                contentDescription = "AddLink",
-                                modifier = Modifier.size(28.dp)
-                            )
-                        }
-
-                        IconButton(
-                            enabled = state.selectedFile.isBookmark,
-                            onClick = {
-                                state.selectedFile.asBookmark?.let { openUrl(it.url) }
+                            IconButton(enabled = true, onClick = { showDeleteDialog = true }) {
+                                Icon(
+                                    imageVector = Icons.Default.DeleteSweep,
+                                    contentDescription = "Delete",
+                                    modifier = Modifier.size(24.dp)
+                                )
                             }
-                        ) {
-                            Icon(
-                                painter = painterResource("browser.png"),
-                                contentDescription = "Open in browser",
-                                modifier = Modifier.size(28.dp)
-                            )
+
+                            IconButton(onClick = { showEditDialog = true }) {
+                                Icon(
+                                    imageVector = Icons.Default.EditNote,
+                                    contentDescription = "Edit",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+
+                            IconButton(
+                                enabled = state.selectedFile.isDirectory,
+                                onClick = { showBookmarkDialog = true }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.AddLink,
+                                    contentDescription = "AddLink",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+
+                            IconButton(
+                                enabled = state.selectedFile.isDirectory,
+                                onClick = { showDirectoryDialog = true }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.CreateNewFolder,
+                                    contentDescription = "AddLink",
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
                         }
                     }
                 },
